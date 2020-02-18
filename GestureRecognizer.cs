ef792682b4634da1ffd961d312c4a5549f8d464b
jorgejgnz <60674219+jorgejgnz@users.noreply.github.com>
2020-02-18 01:06:36 +0100

    Initial commit
    Script to help devs to record and recognize hand tracking gestures easily

---
+++ GestureRecognizer.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.Serializable]
+public struct Gesture
+{
+    public string gestureName;
+    public List<Vector3> positionsPerFinger; // Relative to hand
+    public UnityEvent onRecognized;
+
+    public Gesture(string name, List<Vector3> positions, UnityEvent onRecognized) {
+        this.gestureName = name;
+        this.positionsPerFinger = positions;
+        this.onRecognized = onRecognized;
+    }
+
+    public Gesture(string name, List<Vector3> positions)
+    {
+        this.gestureName = name;
+        this.positionsPerFinger = positions;
+        this.onRecognized = new UnityEvent();
+    }
+
+}
+
+[InitializeOnLoadAttribute]
+public class GestureRecognizer : MonoBehaviour
+{
+    [SerializeField]
+    public List<Gesture> savedGestures = new List<Gesture>();
+
+    public float theresold = 0.1f;
+
+    public UnityEvent onNothindDetected;
+
+    [HideInInspector]
+    public Gesture gestureDetected;
+    bool sthWasDetected = false;
+    
+    [Header("Objects")]
+    public GameObject hand;
+    public GameObject[] fingers;
+
+    [Header("Debugging")]
+    public string gestureNameDetected = "";
+
+    [Header("Saving")]
+    public string gestureName = "";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gestureDetected = Recognize();
+
+        gestureNameDetected = gestureDetected.gestureName;
+
+        if (gestureDetected.Equals(new Gesture()) && sthWasDetected)
+        {
+            sthWasDetected = false;
+            onNothindDetected.Invoke();
+        }else if (!gestureDetected.Equals(new Gesture()) && !sthWasDetected)
+        {
+            sthWasDetected = true;
+            gestureDetected.onRecognized.Invoke();
+        }
+    }
+
+    public void SaveAsGesture()
+    {
+        Vector3 fingerRelativePos;
+
+        Gesture g = new Gesture();
+        g.gestureName = gestureName;
+
+        List<Vector3> positions = new List<Vector3>();
+        for (int i = 0; i < fingers.Length; i++)
+        {
+            fingerRelativePos = fingers[i].transform.position - hand.transform.position;
+            positions.Add(fingerRelativePos);
+        }
+
+        g.positionsPerFinger = positions;
+
+        savedGestures.Add(g);
+
+    }
+
+    public Gesture Recognize()
+    {
+        Vector3 fingerRelativePos;
+        bool discardGesture = false;
+        float sumDistances;
+        float minSumDistances = Mathf.Infinity;
+        Gesture bestCandidate = new Gesture();
+
+        // For each gesture
+        for (int i = 0; i < savedGestures.Count; i++)
+        {
+            // If the number of fingers does not match, it returns an error
+            if (fingers.Length != savedGestures[i].positionsPerFinger.Count) throw new Exception("Different number of tracked fingers");
+
+            sumDistances = 0f;
+
+            // For each finger
+            for (int j = 0; j < fingers.Length; j++)
+            {
+                fingerRelativePos = fingers[j].transform.position - hand.transform.position;
+
+                // If at least one finger does not enter the theresold we discard the gesture
+                if (Vector3.Distance(fingerRelativePos, savedGestures[i].positionsPerFinger[j]) > theresold)
+                {
+                    discardGesture = true;
+                    break;
+                }
+
+                // If all the fingers entered, then we calculate the total of their distances
+                sumDistances += Vector3.Distance(fingerRelativePos, savedGestures[i].positionsPerFinger[j]);
+            }
+
+            // If we have to discard the gesture, we skip it
+            if (discardGesture)
+            {
+                discardGesture = false;
+                continue;
+            }
+
+            // If it is valid and the sum of its distances is less than the existing record, it is replaced because it is a better candidate 
+            if (sumDistances < minSumDistances)
+            {
+                minSumDistances = sumDistances;
+                bestCandidate = savedGestures[i];
+            }
+        }
+
+        // If we've found something, we'll return it
+        // If we haven't found anything, we return it anyway (newly created object)
+        return bestCandidate;
+    }
+
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(GestureRecognizer))]
+public class CustomInspector_GestureRecognizer : Editor
+{
+
+    private void OnEnable()
+    {
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GestureRecognizer myScript = (GestureRecognizer)target;
+        if (GUILayout.Button("Save current gesture"))
+        {
+            myScript.SaveAsGesture();
+            OnInspectorGUI();
+        }
+
+        
+
+    }
+}
+#endif
---
+++ README.md
@@ -0,0 +1,51 @@
+# Industrial Tech Demo (Oculus Quest)
+
+Unity utility to help devs to record hand tracking gestures easier
+
+![Basic desired vbehaviour](http://url/to/img.png)
+
+![Recording hand tracking gestures](http://url/to/img.png)
+
+## Getting Started
+
+How can I use this?
+
+1. Download GestureRecognizer.cs
+2. Move it to your project folder
+3. Make custom gameobjects to follow each finger tip as well as the base of the hand
+4. Attach GestureRecognizer component to any object in the Scene
+5. Set Hand with the gameobject taht follows base of the Hand
+6. Set the array of gameobjects with the gameobjects that follow each finger tip
+7. Connect your Oculus Quest with Oculus Link
+
+8. Connect and wake up your Oculus Quest
+9. Start Oculus Link
+
+10. Start Play Mode in Unity
+11. Make the desired gesture with your hand
+12. Pause Play Mode
+13. Press "Save current gesture" in Inspector
+14. Copy the GestureRecognizer component
+15. Stop Play Mode
+16. Paste Component Values in the same GestureRecognizer component
+
+17. Deploy the new Gesture object added to the list of Saved Gestures
+18. Fill the onRecognize event to perform desired actions when gesture is detected
+
+## Built With
+
+This Unity project uses the following:
+
+* [Unity 2019.2.9f1](https://unity3d.com/es/get-unity/download/archive)
+* [Oculus Integration 13.0](https://assetstore.unity.com/packages/tools/integration/oculus-integration-82022)
+* Oculus Link needed
+
+This script can be used without Oculus Integration as it can record and recognize any configuration of positions in a tree of objects.
+
+## Author
+
+* **Jorge Juan González** - *HCI Researcher at I3A (UCLM)* - [GitHub](https://github.com/jormaje) - [LinkedIn](https://www.linkedin.com/in/jorgejgnz/) - [ResearchGate](https://www.researchgate.net/profile/Jorge_Juan_Gonzalez)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
