dea8a364818ee6bdb1c325019f6cf3745f92bcca
jorgejgnz <60674219+jorgejgnz@users.noreply.github.com>
2020-02-19 22:58:29 +0100

    Script cleaning

--- GestureRecognizer.cs
+++ GestureRecognizer.cs
@@ -153,28 +153,12 @@ public class GestureRecognizer : MonoBehaviour
         return bestCandidate;
     }
 
-    Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles) {
-
-       Vector3 dir = point - pivot; // get point direction relative to pivot
-
-       dir = Quaternion.Euler(angles) * dir; // rotate it
-
-       point = dir + pivot; // calculate rotated point
-
-       return point; // return it
-     }
-
 }
 
 #if UNITY_EDITOR
 [CustomEditor(typeof(GestureRecognizer))]
 public class CustomInspector_GestureRecognizer : Editor
 {
-
-    private void OnEnable()
-    {
-
-    }
 
     public override void OnInspectorGUI()
     {
